3bdddb39824b17afb8bb45914e6da4a3903753b7
sarthakpanwar <exclusivethought@gmail.com>
2023-09-10 14:03:22 +0530

    Setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl text-green-400">
+      <h1>
+        netflix GPT creating by own
+      </h1>
+      
+    </div>
+  );
+}
+
+export default App;
