c9352f64b806625b6d86d88001d5f79c2bc33b1b
sarthakpanwar <exclusivethought@gmail.com>
2023-09-13 02:26:13 +0530

    complete css

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -6,15 +6,15 @@ import { BG_URL } from '../utils/constants';
 
 const GptSearch = () => {
   return (
-    <div>
-        <div className='fixed -z-10'>
-        <img className=''src={BG_URL}
-          alt="logo"
-        />
+    <>
+      <div className="fixed -z-10">
+        <img className="h-screen object-cover" src={BG_URL} alt="logo" />
       </div>
+      <div className="">
         <GptSearchBar />
         <GptMovieSuggestions />
-         </div>
+      </div>
+    </>
   )
          
   
--- src/components/Header.js
+++ src/components/Header.js
@@ -80,8 +80,8 @@ const Header = () => {
 
 
   return (
-    <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black  z-10 flex  justify-between  '>
-      <img className='w-44  '
+    <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between  '>
+      <img className='w-44 mx-auto md:mx-0 '
         src={LOGO}
         alt='logo' />
 
@@ -97,12 +97,12 @@ const Header = () => {
 
           </select>)
           }
-          <button className='py-2 px-4  bg-yellow-800 text-white mx-4 my-2 rounded-lg' onClick={handlerGptSearchClick}
+          <button className='py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg' onClick={handlerGptSearchClick}
           >
           {showGptSearch ? "HomePage" : " GPT SEARCH"}
           </button>
 
-          <img className=' w-12 h-12" '
+          <img className=' hidden md:block w-12 h-12 '
             src={user?.photoURL}
             alt='user-icon' />
 
